f331d4508a6a94417203dc7fd01298cdecf71adb
Marcelo Moreira <celomagik@hotmail.com>
2026-07-26 17:29:11 -0300

    adicionado classes usuario,endereco,telefone,exceptios e spring security

---
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,29 @@
+package com.javanauta.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ddd",length = 3)
+    private  String ddd;
+    @Column(name = "numero",length = 9)
+    private String numero;
+    @Column(name = "usuario_id")
+    private Long usuario_id;
+
+}
